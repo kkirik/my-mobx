81a428823874630a4656954978cd8fa0eee8c4f5
Kirill Kharitonov <kkharitonov@phoenixit.ru>
2019-01-10 12:05:57 +0300

    Added babel config

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
-const path = require("path");
+const path = require('path');
 
-const HtmlPlugin = require("html-webpack-plugin");
+const HtmlPlugin = require('html-webpack-plugin');
 
 const root = process.cwd();
 
 module.exports = {
-  devtool: "source-map",
+  devtool: 'source-map',
   bail: false,
-  mode: "development",
-  entry: path.join(__dirname, "src", "Init.tsx"),
+  mode: 'development',
+  entry: path.join(__dirname, 'src', 'Init.tsx'),
   output: {
-    filename: "assets/[name].js",
-    chunkFilename: "assets/[name].js",
-    path: path.resolve(__dirname, "build"),
-    publicPath: "/"
+    filename: 'assets/[name].js',
+    chunkFilename: 'assets/[name].js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/'
   },
   module: {
     rules: [
       {
         test: /.tsx?$/,
-        loader: "babel-loader",
-        include: [path.resolve(__dirname, "src")],
-        exclude: [path.resolve(__dirname, "node_modules")]
+        loader: 'babel-loader',
+        include: [path.resolve(__dirname, 'src')],
+        exclude: [path.resolve(__dirname, 'node_modules')]
       }
     ]
   },
   plugins: [new HtmlPlugin()],
   resolve: {
-    extensions: [".json", ".js", ".ts", ".tsx", ".css"],
-    modules: [root, path.resolve(root, "src"), "node_modules"]
+    extensions: ['.json', '.js', '.ts', '.tsx', '.css'],
+    modules: [root, path.resolve(root, 'src'), 'node_modules']
   },
   devServer: {
-    port: 3000
+    port: 3001
   }
 };
